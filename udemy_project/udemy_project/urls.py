c6ead20524185ef980029dc578f8fbcf9de2c9d1
Admin <mrrneo@outlook.com>
2025-05-20 13:23:54 +0600

    update urls.py

--- udemy_project/udemy_project/urls.py
+++ udemy_project/udemy_project/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
+    path('', include('udemy_app.urls'))
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
