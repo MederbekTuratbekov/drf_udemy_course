8d1539f92540135c0e44fc8d5b8a5293ad2426a4
Admin <mrrneo@outlook.com>
2025-05-20 16:44:11 +0600

    update views.py

--- udemy_project/udemy_app/urls.py
+++ udemy_project/udemy_app/urls.py
@@ -1,14 +1,20 @@
 from rest_framework import routers
 from django.urls import path, include
-from .views import UserProfileListAPIView, CategoryListAPIView, CourseListApiView, LessonListApiView, CertificateListApiView, ReviewListApiView, CartItemsListApiView, FavoriteItemsListApiView
+from .views import (UserProfileViewSet, CategoryListAPIView, CourseListApiView, LessonListApiView, CertificateListApiView,
+                    ReviewListApiView, CartItemsListApiView, FavoriteItemsListApiView)
 
 
 router = routers.SimpleRouter()
-router.register(r'user', UserProfileListAPIView, basename='userprofile_crud')
+router.register(r'users', UserProfileViewSet, basename='userprofile')
 
 
-urlspatterns = [
+urlpatterns = [
     path('', include(router.urls)),
-    # path('', CategoryListAPIView.as_view(), name='movies'),
-    # path('<int:pk>/', CategoryListAPIView.as_view(), name='movie_details'),
-    ]
+    path('categories/', CategoryListAPIView.as_view(), name='category_list'),
+    path('courses/', CourseListApiView.as_view(), name='course_list'),
+    path('lessons/', LessonListApiView.as_view(), name='lesson_list'),
+    path('certificates/', CertificateListApiView.as_view(), name='certificate_list'),
+    path('reviews/', ReviewListApiView.as_view(), name='review_list'),
+    path('cart-items/', CartItemsListApiView.as_view(), name='cart_items_list'),
+    path('favorite-items/', FavoriteItemsListApiView.as_view(), name='favorite_items_list'),
+]
