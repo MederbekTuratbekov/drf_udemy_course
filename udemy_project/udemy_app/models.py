5729cdd026cf56d09dc1dc1c300000ce4b03229d
Admin <mrrneo@outlook.com>
2025-05-20 12:14:29 +0600

    update models.py

--- udemy_project/udemy_app/models.py
+++ udemy_project/udemy_app/models.py
@@ -13,6 +13,7 @@ class UserProfile(AbstractUser):
     def __str__(self):
         return f'{self.first_name} - {self.role}'
 
+# ссылки и их названия
 class Network(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     network_name = models.CharField(max_length=30, blank=True, null=True)
@@ -21,12 +22,14 @@ class Network(models.Model):
     def __str__(self):
         return self.network_name
 
+# категория курсов
 class Category(models.Model):
     category_name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return self.category_name
 
+# курс
 class Course(models.Model):
     category = models.ManyToManyField(Category)
     course_name = models.CharField(max_length=50)
@@ -38,11 +41,13 @@ class Course(models.Model):
     level = models.CharField(choices=CHOICES_LEVEL)
     price = models.PositiveIntegerField()
     created_by = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    # created_at = models.DateField()
+    # created_at = models.DateField() # агай озу коргозуп берем деген кандай реализация кылынаарын
+    # updated_at =  # агай озу коргозуп берем деген кандай реализация кылынаарын
 
     def __str__(self):
         return f'{self.course_name}: {self.price}'
 
+# урок курса
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     lesson_title = models.CharField(max_length=50)
@@ -53,25 +58,29 @@ class Lesson(models.Model):
     def __str__(self):
         return self.lesson_title
 
+# домашний задание
 class Assignment(models.Model):
     assignment_title = models.TextField()
     assignment_description = models.TextField()
     due_date = models.DateField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    student = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    students = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.assignment_title
 
+# экзамен
 class Exam(models.Model):
-    exam_name = models.TextField()
+    exam_title = models.TextField()
     course = models.ManyToManyField(Course)
     passing_score = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
-    duration = models.DurationField()
+    duration = models.TimeField()
+    is_free = models.BooleanField() # платный/бесплатный
 
     def __str__(self):
-        return self.exam_name
+        return self.exam_title
 
+# вопросы экзамена
 class Questions(models.Model):
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=50)
@@ -79,6 +88,7 @@ class Questions(models.Model):
     def __str__(self):
         return self.question_text
 
+# вариянты ответов вопроса
 class Options(models.Model):
     questions = models.ForeignKey(Options, on_delete=models.CASCADE)
     option_text = models.CharField(max_length=30)
@@ -87,8 +97,9 @@ class Options(models.Model):
     def __str__(self):
         return self.option_text
 
+# сертификат если успешно пройден курс
 class Certificate(models.Model):
-    student = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    students = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     issued_at = models.DateField(auto_now_add=True)
     certificate_url = models.FileField()
@@ -96,6 +107,7 @@ class Certificate(models.Model):
     def __str__(self):
         return 'It is Certificates'
 
+# студенты могут оставить отзыв на курсы, но преподаватели не могут оставить отзыв на свой курс
 class Review(models.Model):
     owner_review = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -106,12 +118,14 @@ class Review(models.Model):
     def __str__(self):
         return f'{self.rating} - {self.comment}'
 
+# корзину подключаем к владельцу корзины
 class Cart(models.Model):
     cart_owner = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.cart_owner}'
 
+# всё что есть в корзине
 class CartItems(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
@@ -119,12 +133,14 @@ class CartItems(models.Model):
     def __str__(self):
         return self.course
 
+# подключаем избранные(курсы) к пользователью
 class Favorite(models.Model):
     favorite_owner = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.favorite_owner}'
 
+# все избранные курсы
 class FavoriteItems(models.Model):
     favorite = models.ForeignKey(Favorite, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
