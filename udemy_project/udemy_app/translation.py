5aa06dcddcb391034466c84f1bb68cd51bb10145
Admin <mrrneo@outlook.com>
2025-05-20 23:06:37 +0600

    update translatoin.py

--- udemy_project/udemy_app/translation.py
+++ udemy_project/udemy_app/translation.py
@@ -3,7 +3,7 @@ from .models import UserProfile, Category, Course, Lesson, Assignment, Exam, Que
 
 
 @register(UserProfile)
-class TeacherProfileTranslationOptions(TranslationOptions):
+class UserProfileTranslationOptions(TranslationOptions):
     fields = ('bio',)
 
 @register(Category)
