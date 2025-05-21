47c562b03d9772a699fb8f4fdd10197da073b33c
Admin <mrrneo@outlook.com>
2025-05-21 07:15:18 +0600

    update urls.py

--- udemy_project/udemy_project/urls.py
+++ udemy_project/udemy_project/urls.py
@@ -9,7 +9,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('udemy_app.urls')),
-    # path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
