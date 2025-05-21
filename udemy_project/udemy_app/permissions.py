694fa45d47fa944503967251d9fb5a069efb1ee9
Admin <mrrneo@outlook.com>
2025-05-21 13:16:09 +0600

    update udemy_app

--- udemy_project/udemy_app/permissions.py
+++ udemy_project/udemy_app/permissions.py
@@ -1,6 +1,10 @@
 from rest_framework import permissions
 
 
-class CreatePermissions(permissions.BasePermission):
+class CreateReviewPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == 'student'
+
+class CreateCoursePermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.role == 'teacher'
