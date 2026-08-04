fa231759f3bfc153fb936386a0e970ac7792e1b6
MederbekTuratbekov <142796921+MederbekTuratbekov@users.noreply.github.com>
2026-08-05 03:24:31 +0600

    ...

--- udemy_project/udemy_app/admin.py
+++ udemy_project/udemy_app/admin.py
@@ -20,8 +20,8 @@ class ExamAdmin(NestedModelAdmin, TranslationAdmin):
     inlines = [QuestionsInlines]
     class Media:
         js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
             'modeltranslation/js/tabbed_translation_fields.js',
         )
         css = {
@@ -33,8 +33,8 @@ class ExamAdmin(NestedModelAdmin, TranslationAdmin):
 class TranslateAdmin(TranslationAdmin):
     class Media:
         js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
             'modeltranslation/js/tabbed_translation_fields.js',
         )
         css = {
